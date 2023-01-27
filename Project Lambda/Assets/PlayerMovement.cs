e28f2e3b01f637a5f87e5dfadc05cf959d7a9c01
Minseo Kim <76415166+minsharky@users.noreply.github.com>
2023-01-27 13:37:04 -0600

    updated version of unity

---
+++ Project Lambda/Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*Player movement
+Space Bar: Shoot bullet
+AWSD/UpDown : Move
+Mouse: Aim*/
+
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
