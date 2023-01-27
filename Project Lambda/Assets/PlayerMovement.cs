6b48727562ca82cd077480d7544d2847cbf66246
AkashDeoNU <103459323+AkashDeoNU@users.noreply.github.com>
2023-01-27 13:58:20 -0600

    PlayerMovement
    Adding a component, which can be attached to an object, that permits the player to control the object

--- Project Lambda/Assets/PlayerMovement.cs
+++ Project Lambda/Assets/PlayerMovement.cs
@@ -8,17 +8,40 @@ AWSD/UpDown : Move
 Mouse: Aim*/
 
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Move constant
+    float moveConstant = 0.025f; 
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Left Arrow
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Move the Player to the left
+            transform.Translate(Vector3.left * moveConstant);
+        }
+
+        // Right Arrow
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // Move the Player to the right
+            transform.Translate(Vector3.right * moveConstant);
+        }
+
+        // Up Arrow
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // Move the Player to the up
+            transform.Translate(Vector3.up * moveConstant);
+        }
+
+        // Down Arrow
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // Move the Player to the down
+            transform.Translate(Vector3.down * moveConstant);
+        }
     }
 }
