3b545976452c39f7ca88339897e4ac0712f199ae
AkashDeoNU <103459323+AkashDeoNU@users.noreply.github.com>
2023-02-03 20:03:40 -0600

    Fixed player speed upgrade compilation errors

--- Project Lambda/Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Project Lambda/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -34,28 +34,28 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             // Move the Player to the left
-            transform.position = new Vector3(transform.position.x - moveConstant, transform.position.y);
+            transform.position = new Vector3(transform.position.x - moveSpeed, transform.position.y);
         }
 
         // Right Arrow
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             // Move the Player to the right
-            transform.position = new Vector3(transform.position.x + moveConstant, transform.position.y);
+            transform.position = new Vector3(transform.position.x + moveSpeed, transform.position.y);
         }
 
         // Up Arrow
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
             // Move the Player to the up
-            transform.position = new Vector3(transform.position.x, transform.position.y + moveConstant);
+            transform.position = new Vector3(transform.position.x, transform.position.y + moveSpeed);
         }
 
         // Down Arrow
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
         {
             // Move the Player to the down
-            transform.position = new Vector3(transform.position.x, transform.position.y - moveConstant);
+            transform.position = new Vector3(transform.position.x, transform.position.y - moveSpeed);
         }
 
         //aims player to mouse position
